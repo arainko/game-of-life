d9f6e126bde478dbf7379da143da381ad7a7b2e7
Aleksander Rainko <aleksander.rainko99@gmail.com>
2019-04-06 01:15:24 +0200

    basic functionality achieved

--- life.scala
+++ life.scala
@@ -34,47 +34,65 @@ object life extends App{
             val rightEdge = board.head.size-1
             val bottomEdge = board.size-1 
 
-            val value = (row, col) match {
-                case (0,0) => { // left upper corner
-                    board(row-1)(col) + board(row-1)(col+1) + board(row)(col+1) 
+            if (row == board.size) board
+            else {
+                val value = (row, col) match {
+                    case a if (row == 0 && col == 0) => { // left upper corner
+                        board(row+1)(col) + board(row+1)(col+1) + board(row)(col+1) 
+                    }
+                    case b if (row == bottomEdge && col ==  0) => { // left bottom corner
+                        board(row)(col+1) + board(row-1)(col+1) + board(row-1)(col)     
+                    }
+                    case c if (row == bottomEdge && col == rightEdge) => { // right bottom corner
+                        board(row-1)(col) + board(row-1)(col-1) + board(row)(col-1) 
+                    }
+                    case d if (row == 0 && col == rightEdge) => { // right upper corner
+                       board(row)(col-1) + board(row+1)(col-1) + board(row+1)(col) 
+                    }
+                    case e if (row == 0) => { // top edge
+                        board(row)(col-1) + board(row+1)(col-1) + board(row+1)(col) + board(row+1)(col+1) + board(row)(col+1)
+                    }
+                    case f if (col ==  0) => { // left edge
+                        board(row+1)(col) + board(row+1)(col+1) + board(row)(col+1) + board(row-1)(col+1) + board(row-1)(col)
+                    }
+                    case g if (row == bottomEdge) => { // bottom edge
+                        board(row)(col+1) + board(row-1)(col+1) + board(row-1)(col) + board(row-1)(col-1) + board(row)(col-1)
+                    }
+                    case h if (col == rightEdge) => { // right edge
+                        board(row-1)(col) + board(row-1)(col-1) + board(row)(col-1) + board(row+1)(col-1) + board(row+1)(col)
+                    }
+                    case _ => {
+                       board(row+1)(col) + board(row+1)(col-1) + board(row)(col-1) + board(row-1)(col-1) + board(row-1)(col) + board(row-1)(col+1) + board(row)(col+1) + board(row+1)(col+1)
+                     }
                 }
-                case (bottomEdge, 0) => { // left bottom corner
-                    board(row)(col+1) + board(row+1)(col+1) + board(row+1)(col)     
-                }
-                case (bottomEdge, rightEdge) => { // right bottom corner
-                    board(row+1)(col) + board(row+1)(col-1) + board(row)(col-1) 
-                }
-                case (0, rightEdgeCord) => { // right upper corner
-                    board(row)(col-1) + board(row-1)(col-1) + board(row-1)(col) 
-                }
-                case (0, _) => { // top edge
-                    board(row)(col-1) + board(row-1)(col-1) + board(row-1)(col) + board(row-1)(col+1) + board(row)(col+1)
-                }
-                case (_, 0) => { // left edge
-                    board(row-1)(col) + board(row-1)(col+1) + board(row)(col+1) + board(row+1)(col+1) + board(row+1)(col)
-                }
-                case (bottomEdge, _) => { // bottom edge
-                    board(row)(col+1) + board(row+1)(col+1) + board(row+1)(col) + board(row+1)(col-1) + board(row)(col-1)
-                }
-                case (_, rightEdge) => { // right edge
-                    board(row+1)(col) + board(row+1)(col-1) + board(row)(col-1) + board(row-1)(col-1) + board(row-1)(col)
-                }
-                case (_, _) => {
-                    board(row+1)(col) + board(row+1)(col-1) + board(row)(col-1) + board(row-1)(col-1) + board(row-1)(col) + board(row-1)(col+1) + board(row)(col+1) + board(row+1)(col+1)
-                }
-            }
 
-            board(row)(col) match {
-                case 0 => 
-                case 1 => 
+                val updatedBoard = board(row)(col) match {
+                    case 0 => value match{
+                        case 3 => board.updated(row, board(row).updated(col, 1))
+                        case _ => board
+                    }
+                    case 1 => value match {
+                        case a if (value == 0 || value == 1 || (value > 3)) => board.updated(row, board(row).updated(col, 0))
+                        case _ => board
+                    }
+                }
+
+                col match {
+                    case a if (col == rightEdge) => stateHelper(updatedBoard, row+1, 0)
+                    case _ => stateHelper(updatedBoard, row, col+1)
+                }
             }
         }
+        stateHelper(board, 0, 0)
     }
 
+    def main() = ???
 
 
-
-    //printBoard(initBoardState(30,10).map(n => n.map(k => if (k == 1) '#' else ' ')))
+    val init = initBoardState(30,10)//.map(n => n.map(k => if (k == 1) '#' else ' '))
+    val next = nextBoardState(init)
+    printBoard(init.map(n => n.map(k => if (k == 1) '#' else ' ')))
+    printBoard(next.map(n => n.map(k => if (k == 1) '#' else ' ')))
     //print("\u001b[2J")
     
 }
